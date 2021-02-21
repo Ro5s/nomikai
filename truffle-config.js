0a0d09f8c0ab291f7d8e065ff0ee0d85a651c4b3
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-02-20 20:19:26 -0500

    Create truffle-config.js

---
+++ truffle-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  compilers: {
+    solc: {
+      version: '0.5.3',
+      settings: {
+        optimizer: {
+          enabled: true, // Default: false
+          runs: 200      // Default: 200
+        },
+      }
+    }
+  },
+  
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 8545,
+      gas: 6721975, // <-- Use this high gas value
+      gasPrice: 1000000000,    // <-- Use this low gas price
+      network_id: '*', // Match any network id
+    },
+  },
+
+  mocha: {
+    enableTimeouts: false,
+    before_timeout: 120000 // 2min
+  }
+}
